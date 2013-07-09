268b3375076e6ba5fcff0b3df0e63600e2a0a80d
Albert33 <albert.llairo 2gmail.com>
2013-07-09 18:49:47 +0200

    Funcions 1/3 acabat

--- Introduccio funcions.js
+++ Introduccio funcions.js
@@ -3,3 +3,30 @@ var greeting = function (name) {
 };
 
 greeting("Albert");
+
+// Write your foodDemand function below.
+// Last hint: In your reusable block of code, end each line
+// with a semicolon (;)
+var foodDemand = function (food){
+    console.log("I want to eat" + " " + food);
+};
+foodDemand("fried eggs");
+
+// Parameter is a number, and we do math with that parameter
+var timesTwo = function(number) {
+    return number * 2;
+};
+var newNumber = timesTwo(4);
+console.log(newNumber);
+
+// Define quarter here.
+var quarter = function (number){
+    return number/4;
+};
+
+
+if (quarter(12) % 3 === 0 ) {
+  console.log("The statement is true");
+} else {
+  console.log("The statement is false");
+}
