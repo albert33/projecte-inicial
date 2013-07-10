db010edcfaf7a7b9bc431f6f8c48d6711d948a93
Arnau Vàzquez <arnau@softwaremeetsdesign.com>
2013-07-10 15:18:47 +0200

    Nova tasca 2, eliminació fitxers temporals.

---
+++ Tasca 2.js
@@ -0,0 +1,51 @@
+/*
+ * 1. Defineix una funció max() que accepti dos nombres com a paràmetres i en retorni
+ * el més gran. Utilitza l'estructura if-else de JavaScript
+ */
+
+/*
+ * 2. Defineix una funció maxOfThree() que accepti tres nombres com a paràmetres i
+ * en retorni el més gran. Reutilitza funcions anteriors.
+ */
+
+/*
+ * 3. Escriu una funció isVowel() que accepti un caracter (una frase de longitud 1) i
+ * retorni el booleà 'true' si és una vocal, 'false' si és una consonant.
+ */
+
+/*
+ * 4. Escriu una funció translate() que tradueixi les frases en un idioma inventat.
+ * Les vocals s'han de repetir amb la lletra 'p' entremig. Per exemple:
+ * "Un bon dia" esdevindria "Upun bopon dipiapa".
+ */
+
+/*
+ * 5. Defineix una funció sum() i una multiply() que retornin el resultat de
+ * sumar o multiplicar respectivament tots els nombres en l'array donat com a paràmetre.
+ * Per exemple, sum([1,2,3,4]) hauria de retornar 10.
+ */
+
+
+/*
+ * 6. Defineix una funció reverse() que doni la volta a una frase.
+ * Per exemple, reverse("Un bon dia") seria "Aid nob nu".
+ */
+
+/*
+ * 7. Escriu una funció findLongestWord() que prengui un array de paraules
+ * i retorni la longitud de la més llarga.
+ */
+
+/*
+ * 8. Escriu una funció filterLongWords() que prengui com a paràmetres un
+ * array de paraules i un enter len, i retorni l'array de paraules que són
+ * més llargues o iguals que len.
+ */
+
+/*
+ * 9. Escriu una funció charFreq() que prengui una cadena de text i construeixi
+ * un llistat de freqüència d'aparició de cada caràcter de la cadena.
+ * Representa el llistat com un Objecte Javascript, on a la propietat "a" li correspon
+ * un enter que és el nombre de vegades que apareix aquesta lletra a la cadena d'entrada.
+ * Si una lletra no apareix no cal que estigui definida a l'objecte.
+ */
